c977da001f4fd5fa74a3a830ce66cb3c202a6228
Lucas <Polo>
2021-09-15 21:01:22 -0300

    feat: capítulo 13: Inheritance

--- src/main/java/dev/lucaspolo/lox/LoxClass.java
+++ src/main/java/dev/lucaspolo/lox/LoxClass.java
@@ -5,9 +5,11 @@ import java.util.Map;
 
 public class LoxClass implements LoxCallable{
     final String name;
+    final LoxClass superclass;
     private final Map<String, LoxFunction> methods;
 
-    LoxClass(String name, Map<String, LoxFunction> methods) {
+    LoxClass(String name, LoxClass superclass, Map<String, LoxFunction> methods) {
+        this.superclass = superclass;
         this.name = name;
         this.methods = methods;
     }
@@ -16,6 +18,11 @@ public class LoxClass implements LoxCallable{
         if (methods.containsKey(name)) {
             return methods.get(name);
         }
+
+        if (superclass != null) {
+            return superclass.findMethod(name);
+        }
+
         return null;
     }
 
--- src/main/java/dev/lucaspolo/lox/Parser.java
+++ src/main/java/dev/lucaspolo/lox/Parser.java
@@ -41,6 +41,14 @@ public class Parser {
 
     private Stmt classDeclaration() {
         Token name = consume(TokenType.IDENTIFIER, "Expect class name");
+
+        Expr.Variable superclass = null;
+
+        if (match(TokenType.LESS)) {
+            consume(TokenType.IDENTIFIER, "Expect superclass name.");
+            superclass = new Expr.Variable(previous());
+        }
+
         consume(TokenType.LEFT_BRACE, "Expect '{' before class body.");
 
         List<Stmt.Function> methods = new ArrayList<>();
@@ -50,7 +58,7 @@ public class Parser {
 
         consume(TokenType.RIGHT_BRACE, "Expect '}' after class body.");
 
-        return new Stmt.Class(name, methods);
+        return new Stmt.Class(name, superclass, methods);
     }
 
     private Stmt.Function function(String kind) {
@@ -341,6 +349,13 @@ public class Parser {
 
         if (match(TokenType.NUMBER, TokenType.STRING)) {
             return new Expr.Literal(previous().literal);
+        }
+
+        if (match(TokenType.SUPER)) {
+            Token keyworkd = previous();
+            consume(TokenType.DOT, "Expect '.' after 'super'.");
+            Token method = consume(TokenType.IDENTIFIER, "Expect superclass method name.");
+            return new Expr.Super(keyworkd, method);
         }
 
         if (match(TokenType.THIS)) return new Expr.This(previous());
